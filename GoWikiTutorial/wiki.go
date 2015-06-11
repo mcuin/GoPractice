1705efb62486fc9028d32f071b2ab253a3f0ced3
Mykal Cuin <mykcuin@gmail.com>
2015-06-11 14:30:45 -0600

    Writing Web Applications: Handling Absent Pages

--- GoWikiTutorial/wiki.go
+++ GoWikiTutorial/wiki.go
@@ -27,10 +27,19 @@ func loadPage(title string) (*Page, error) {
 	}
 }
 
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	t, _ := template.ParseFiles(tmpl + ".html")
+	t.Execute(w, p)
+}
+
 func viewHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/view/"):]
-	p, _ := loadPage(title)
-	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+	p, err := loadPage(title)
+	if err != nil {
+		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+		return
+	}
+	renderTemplate(w, "view", p)
 }
 
 func editHandler(w http.ResponseWriter, r *http.Request) {
@@ -39,8 +48,7 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		p = &Page{Title: title}
 	}
-	t, _ := template.ParseFields("edit.html")
-	t.Execute(w, p)
+	renderTemplate(w, "edit", p)
 }
 
 func main() {
