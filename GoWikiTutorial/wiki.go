8f8d48f3a7deffbc013f34002e68b2e29bc33d8e
Mykal Cuin <mykcuin@gmail.com>
2015-06-12 11:32:28 -0600

    Writing Web Applications: Literals and Closures

--- GoWikiTutorial/wiki.go
+++ GoWikiTutorial/wiki.go
@@ -21,14 +21,14 @@ func (p *Page) save() error {
 	return ioutil.WriteFile(filename, p.Body, 0600)
 }
 
-func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
+/*func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
 	m := validPath.FindStringSubMatch(r.URL.Path)
 	if m == nil {
 		http.NotFound(w, r)
 		return "". error.New("Invalid Page Title")
 	}
 	return m[2], nil
-}
+}*/
 
 func loadPage(title string) (*Page, error) {
 	filename := title + ".txt"
@@ -47,11 +47,18 @@ func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
 	}	
 }
 
-func viewHandler(w http.ResponseWriter, r *http.Request) {
-	title, err := getTitle(w, r)
-	if err != nil {
-		return
+func makeHandler(fn func (http.ResponseWriter, *http.Request, string)) http.HandlerFunc  {
+	return func(w http.ResponseWriter, r *http.Request) {
+		m := validPath.FindStringSunmatch(r.URL.Path)
+		if m == nil {
+			http.NotFound(w, r)
+			return
+		}
+		fn(w, r, m[2])
 	}
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
 	p, err := loadPage(title)
 	if err != nil {
 		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
@@ -60,11 +67,7 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 	renderTemplate(w, "view", p)
 }
 
-func editHandler(w http.ResponseWriter, r *http.Request) {
-	title, err := getTitle(w, r)
-	if err != nil {
-		return
-	}
+func editHandler(w http.ResponseWriter, r *http.Request, title string) {
 	p, err := loadPage(title)
 	if err != nil {
 		p = &Page{Title: title}
@@ -72,11 +75,7 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
 	renderTemplate(w, "edit", p)
 }
 
-func saveHandler(w http.ResponseWriter, r *http.Request) {
-	title, err := getTitle(w, r)
-	if err != nil {
-		return
-	}
+func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
 	body := r.FormValue("body")
 	p := &Page(Title: title, Body: []byte(body))
 	err := p.save()
@@ -88,9 +87,24 @@ func saveHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/view/", viewHandler)
-	http.HandleFunc("/edit/", editHandler)
-	http.HandleFunc("/save/", saveHandler)
+	flag.Parse()
+	http.HandleFunc("/view/", makeHandler(viewHandler))
+	http.HandleFunc("/edit/", makeHandler(editHandler))
+	http.HandleFunc("/save/", makeHandler(saveHandler))
+	
+	if *addr {
+		l, err := net.Listen("tcp", "127.0.0.1:0")
+		if err != nil {
+			log.Fatal(err)
+		}
+		err = ioutil.WriteFile("final-port.txt", []byte(l.Addr().String()), 0644) 
+		if err != nil {
+			log.Fatal(err)
+		}
+		s := &http.Server{}
+		s.Serve(l)
+		return
+	}
 	http.ListenAndServe(":8080", nil)
 }
 
