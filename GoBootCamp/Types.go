c02b750a69cd1c688c413348dd74ed622ac3e1b1
Mykal Cuin <mykcuin@gmail.com>
2015-11-04 12:41:18 -0700

    GoBootCamp 3.4 Structs

--- GoBootCamp/Types.go
+++ GoBootCamp/Types.go
@@ -11,6 +11,11 @@ var (
 	goIsFun        = true
 	maxInt  uint64 = 1<<64 - 1
 	complex        = cmplx.Sqrt(-5 + 12i)
+
+	p = Point{1, 2}
+	q = &Point{1, 2}
+	r = Point{X: 1}
+	t = Point{}
 )
 
 func timeMap(y interface{}) {
@@ -18,6 +23,37 @@ func timeMap(y interface{}) {
 	if ok {
 		z["updated at"] = time.Now()
 	}
+}
+
+type Stringer interface {
+	String() string
+}
+
+type fakeString struct {
+	content string
+}
+
+func (s *fakeString) String() string {
+	return s.content
+}
+
+func printString(value interface{}) {
+	switch str := value.(type) {
+	case string:
+		fmt.Println(str)
+	case Stringer:
+		fmt.Println(str.String())
+	}
+}
+
+type BootCamp struct {
+	Lat  float64
+	Lon  float64
+	Date time.Time
+}
+
+type Point struct {
+	X, Y int
 }
 
 func main2() {
@@ -38,4 +74,24 @@ func main2() {
 
 	timeMap(foo)
 	fmt.Println(foo)
+
+	s := &fakeString{"Ceci n'est pas un string"}
+	printString(s)
+	printString("Hello gophers")
+
+	fmt.Println(BootCamp{
+		Lat:  34.012836,
+		Lon:  -118.495338,
+		Date: time.Now(),
+	})
+
+	fmt.Println(p, q, r, t)
+
+	event := BootCamp{
+		Lat: 34.012836,
+		Lon: -118.495338,
+	}
+
+	event.Date = time.Now()
+	fmt.Printf("Event on %s, location (%f, %f)\n", event.Date, event.Lat, event.Lon)
 }
